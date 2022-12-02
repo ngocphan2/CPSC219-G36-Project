7cfcc7c3c07e6cd6e3cc9a378aa17464aeb58359
Henry Pham <phung.pham@ycalgary.ca>
2022-12-01 18:50:25 -0700

    Implement InvalidBudgetxception class to handle errors

---
+++ src/application/InvalidBudgetException.java
@@ -0,0 +1,30 @@
+package application;
+
+public class InvalidBudgetException extends Exception {
+
+	public InvalidBudgetException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public InvalidBudgetException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public InvalidBudgetException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public InvalidBudgetException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public InvalidBudgetException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+}
